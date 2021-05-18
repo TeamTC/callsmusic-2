0dee4428428d2d18de1e4f8e79af79c8ae59ba28
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-05-18 17:34:59 +0530

    fimxems

---
+++ callsmusic/handlers/private.py
@@ -0,0 +1,44 @@
+# Calls Music 2 - Telegram bot for streaming audio in group calls
+# Copyright (C) 2021  Roj Serbest
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import Message
+
+from ..helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""Hi!
+
+I can play song in your channel and group voice chat! (**BETA BOT**)
+
+Need Help? Ask in **@Nexa_bots**
+
+Join @NexaBotsUpdates""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔰️ My Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                    ),
+                    InlineKeyboardButton(
+                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                    )
+                ]
+            ]
+        )
+    )
