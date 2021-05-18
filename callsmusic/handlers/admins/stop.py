d8fcbe8584629408a38fa9c1c147604d0b8bf1a3
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-05-18 17:44:26 +0530

    bye bye

--- callsmusic/handlers/admins/stop.py
+++ callsmusic/handlers/admins/stop.py
@@ -21,11 +21,11 @@ from callsmusic.helpers.filters import command
 from callsmusic.helpers.filters import other_filters
 
 
-@Client.on_message(command('stop') & other_filters)
+@Client.on_message(command('cstop') & other_filters)
 @errors
 @authorized_users_only
 async def _(_, message: Message):
     if stop(message.chat.id):
-        await message.reply_text('Stopped!')
+        await message.reply_text('Stopped Streaming!')
     else:
         await message.reply_text('Nothing is playing!')
