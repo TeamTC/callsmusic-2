c58365e6cd00fe2019b3a4f76519cac98306edf9
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-05-18 17:44:02 +0530

    sad

--- callsmusic/handlers/admins/skip.py
+++ callsmusic/handlers/admins/skip.py
@@ -21,11 +21,11 @@ from callsmusic.helpers.filters import command
 from callsmusic.helpers.filters import other_filters
 
 
-@Client.on_message(command('skip') & other_filters)
+@Client.on_message(command('cskip') & other_filters)
 @errors
 @authorized_users_only
 async def _(_, message: Message):
     if skip(message.chat.id):
-        await message.reply_text('Skipped!')
+        await message.reply_text('Skipped Song!')
     else:
         await message.reply_text('Nothing is playing!')
